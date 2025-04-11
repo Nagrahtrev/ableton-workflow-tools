2cf26009014c143b7ff9600de7a6499131c3075a
Nagrahtrev <122343176+Nagrahtrev@users.noreply.github.com>
2025-04-11 21:31:27 +0800

    UPDATE

--- get-plug-list.py
+++ get-plug-list.py
@@ -1,12 +1,15 @@
 import os
+import datetime
 from tkinter.filedialog import askdirectory
 
 script_dir = os.path.dirname(os.path.realpath(__file__))
 os.chdir(script_dir)
-
 folder_path = askdirectory()
-ext = ['dll', 'vst3', 'aaxplugin', 'clap']
-file = open('mypluglist.txt', 'w')
+now = datetime.datetime.now()
+timestmp = now.strftime("%Y-%m-%d_%H-%M-%S")
+
+txt = open(f"mypluglist_{timestmp}.txt", "w")
+ext = ["dll", "vst3", "aaxplugin", "clap"]
 
 def getFileName(dir, file_list):
     dir_new = dir
@@ -18,14 +21,14 @@ def getFileName(dir, file_list):
             getFileName(dir_new, file_list)
     return file_list
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     def myPlugList(my_path):
         my_list = getFileName(my_path, [])
         for j in my_list:
-            index = j.find('.')
+            index = j.find(".")
             file_ext = j[index+1:]       
             if file_ext in ext:
                 print(j[:index])
-                file.write(j[:index] + '\n')
+                txt.write(j[:index] + "\n")
 
 myPlugList(folder_path)
